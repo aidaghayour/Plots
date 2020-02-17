5b8006eff693d0aa8edf80bb8c6448cfc0e54318
Aida Ghayour <aghayourkhia@mun.ca>
2020-02-17 18:11:19 -0330

    Update Roberto_plots.R

--- Roberto_plots.R
+++ Roberto_plots.R
@@ -8,3 +8,8 @@ center_pos.cebpb <- rowMeans(cebpb[,2:3])
 cebpb_box <- data.frame(pos = center_pos.cebpb, pval = cebpb[,4])
 binMap.cebpb <- cut( cebpb_box$pos, breaks = seq(-200,0, by = 10), labels = seq(-200,-1, by = 10 ))
 boxplot(cebpb_box$pval~binMap.cebpb,ylab="-log10 Pval",xaxt="n",main="CEBPB0nly FPWM, P-Value < 0.00001",ylim=c(3,8),col="green", add = TRUE)
+
+######### Frequency
+ggplot() +
+    geom_freqpoly(data = reverse,aes(X3,size = 'qsec'),binwidth = 5,col="red")+ geom_freqpoly(data = positive,aes(X3,size = 'qsec'),binwidth = 5,col="black") +theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                                                                                                                        panel.background = element_blank(), axis.line = element_line(colour = "black"))
